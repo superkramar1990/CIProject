9384f64b365c4dcf4131cee0c1d13132ba821eb5
Stanislav Kramarenko <>
2019-04-26 01:14:01 +0600

    first

---
+++ CIProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIProject
+//
+//  Created by Stanislav Kramarenko on 26.04.2019.
+//  Copyright © 2019 Stanislav Kramarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
